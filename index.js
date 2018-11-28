ed61bec871af8a2602374a953028fc6338d6f9f8
nahomiyoz <nahomiyoz@gmail.com>
2018-11-28 23:29:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+  add=a+b;
+  a=1
+  b=80
+  return add
+  
+}
+var a=60
+var b=40
+function subtract(a,b){
+  subtract=a-b
+  return subtract
+}
+  
+
+function multiply(a,b){
+  multiply=a*b
+  a=2
+  b=3.4
+  return multiply
+}
+function divide (a,b){
+  divide=a/b
+  a=5
+  b=2.5
+  return divide
+}
+var n=5
+function inc(n){
+  n++
+  return n
+}
+var n=6
+function dec(n){
+  n--
+  return n
+}
